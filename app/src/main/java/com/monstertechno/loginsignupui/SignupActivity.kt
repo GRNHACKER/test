e85607956c24dc22ec3e1d0a7a90ad61f76bfe9b
GRNHACKER <anantkr.rediff@gmail.com>
2022-03-20 22:45:04 +0530

    first commit

---
+++ app/src/main/java/com/monstertechno/loginsignupui/SignupActivity.kt
@@ -0,0 +1,19 @@
+package com.monstertechno.loginsignupui
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.smarteist.autoimageslider.DefaultSliderView
+import com.smarteist.autoimageslider.IndicatorAnimations
+import com.smarteist.autoimageslider.SliderAnimations
+import com.smarteist.autoimageslider.SliderLayout
+
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test
+        )
+    }
+}
